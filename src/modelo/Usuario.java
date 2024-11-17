a19db63e1f96be089de4710414c4aa75e71c9363
SNEIDER31LM <holaquetallooctrex31@gmail.com>
2024-11-17 12:40:37 -0500

    GUI

---
+++ src/modelo/Usuario.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Usuario {
+    private String nombre;
+    private int edad;  // Nuevo atributo para la edad
+
+    // Constructor actualizado
+    public Usuario(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+}
